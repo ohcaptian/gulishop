ef43b80913ae5d986600b6cfef3e4c1c4a9cbebb
yyf <cz_captain@163.com>
2021-05-29 22:17:47 +0800

    代码初级架构搭建，meber服务存在问题，上传后在测试

--- member/src/main/java/com/yin/member/MemberApplication.java
+++ member/src/main/java/com/yin/member/MemberApplication.java
@@ -2,8 +2,15 @@ package com.yin.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+
+/**
+ *远程调用接口
+ */
+@EnableFeignClients(basePackages= "com.yin.member.feign")
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class MemberApplication {
 
     public static void main(String[] args) {
--- product/src/main/java/com/yin/product/ProductApplication.java
+++ product/src/main/java/com/yin/product/ProductApplication.java
@@ -3,6 +3,12 @@ package com.yin.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 整合mybatis-plus
+ * 1：导入依赖
+ * 2：相关信息配置
+ */
 @SpringBootApplication
 public class ProductApplication {
 
